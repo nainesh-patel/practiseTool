1d83b9c52c8b0651ff51c69709358a05fe9f54fc
Janhvi <Janhvi@Nainesh.sdu.local>
2016-12-20 11:00:36 +0530

    first change

---
+++ Practise/src/accretive/HelloWorld.java
@@ -0,0 +1,13 @@
+package accretive;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		
+		System.out.println("hello world");
+		
+		System.out.println("second hello world");
+		System.out.println("third hello world");
+	}
+
+}
---
+++ Practise/src/accretive/New.java
@@ -0,0 +1,11 @@
+package accretive;
+
+public class New {
+
+	public static void main(String[] args) {
+		
+		System.out.println("new file");// TODO Auto-generated method stub
+
+	}
+
+}
